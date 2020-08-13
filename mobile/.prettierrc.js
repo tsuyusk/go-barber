48c1c1468f05ca6db188c9eeea6e6148b9dc6669
tsuy <rafaeldesenhos99@gmail.com>
2020-08-12 21:54:31 -0300

    Changed from name 'mobile' to 'appgobarber'

--- mobile/.prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  jsxBracketSameLine: true,
-  bracketSpacing: true,
-  singleQuote: true,
-  trailingComma: 'all',
-};
--- mobile/src/hooks/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { AuthContextProvider } from './auth';
-
-const RootProvider: React.FC = ({ children }) => {
-  return <AuthContextProvider>{children}</AuthContextProvider>;
-};
-
-export default RootProvider;
